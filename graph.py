ddded79524eae44eab3eddefed0f0bedf21ecf56
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-02-07 19:43:48 +0530

    first

---
+++ graph.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+xpoints = np.array([0.174113, 0.171357, 0.190581, 0.170198])
+ypoints = np.array([0.063519, 0.065683, 0.063368, 0.061566])
+
+plt.plot(xpoints, ypoints)
+plt.show()
+
---
+++ prac1.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printArr(vector <int> arr, int n, string filename){
+    ofstream outputfile(filename);
+    for(int i=0; i<n; i++){
+        outputfile << arr[i]<< " ";
+    }
+    outputfile.close();
+}
+
+void selectionSort(vector <int> arr, int n){
+    for(int i = 0; i<n; i++){
+        int mini = i;
+        for(int j= i+1; j<n; j++)
+            if(arr[j]< arr[mini])
+                mini = j;
+        swap(arr[i], arr[mini]);
+    }
+    printArr(arr, n, "selectionSort.txt");
+}
+
+void bubbleSort(vector <int> arr, int n){
+    for(int i = 0; i<n-1; i++){
+        for(int j = 0; j< n-1-i; j++)
+            if(arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+    }
+    printArr(arr, n, "bubbleSort.txt");
+}
+
+void randomNums(int n){
+    vector <int> arr;
+    ofstream outputfile("input.txt");
+    for(int i=0; i<n; i++){
+        outputfile << rand() % (n+1)<< " ";
+    }
+    outputfile.close();
+
+}
+
+void runner(){
+    randomNums(13); // put number of numbers to generate
+
+    clock_t startTime, endTime;
+    double totalTime;
+
+    vector <int> arr;
+    ifstream readfile("input.txt");
+
+    int value;
+    while(readfile >> value){
+        arr.push_back(value);
+    }
+    readfile.close();
+
+    startTime = clock();
+    bubbleSort(arr, arr.size());
+    endTime = clock();
+
+    totalTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+    cout<<"Bubble Sort Time: "<<totalTime<<endl;
+    
+    startTime = clock();
+    selectionSort(arr, arr.size());
+    endTime = clock();
+
+    totalTime = ((double)(endTime - startTime)) / CLOCKS_PER_SEC;
+    cout<<"Selection Sort Time: "<<totalTime<<endl;
+
+}
+
+int main(){
+
+    runner();
+    runner();
+    runner();
+    runner();
+    
+    return 0;
+}
