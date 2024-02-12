d68ca69c845bbec3886a3bc4028c112e4f33d66c
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2024-02-12 04:28:51 +0000

    update graph

--- graph.py
+++ graph.py
@@ -3,7 +3,7 @@ import numpy as np
 
 num = np.array([1000, 2000, 5000, 7500, 9000])
 
-bubble_y_pts = np.array([0.005726, 0.021891, 0.389512, 0.446441, 0.5422146])
+bubble_y_pts = np.array([0.005726, 0.021891, 0.389512, 0.446441, 0.6422146])
 selection_y_pts = np.array([0.003146,  0.010198,0.094497, 0.162594, 0.188753])
 
 plt.plot(num, bubble_y_pts, label = "Bubble Sort")
